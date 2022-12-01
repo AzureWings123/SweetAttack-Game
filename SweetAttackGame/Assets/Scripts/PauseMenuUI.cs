5a6337bf7f7f0d0a9bf8a7957a30d6637d2081c2
Ricardo D'Angelo Gonzalez <42185864+cardogg@users.noreply.github.com>
2022-12-01 04:50:01 -0800

    Pause menu

--- SweetAttackGame/Assets/Scripts/PauseMenuUI.cs
+++ SweetAttackGame/Assets/Scripts/PauseMenuUI.cs
@@ -5,31 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
-    public static bool GameIsPaused = false;
+    public bool GameIsPaused = false;
 
     public GameObject pauseMenuUI;
 
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            if (GameIsPaused)
-            {
-                Resume();
-            } else
-            {
-                Pause();
-            }
-        }
-    }
-
-    public void Resume ()
+    public void Resume()
     {
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
         GameIsPaused = false;
     }
-    void Pause ()
+    public void Pause()
     {
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
